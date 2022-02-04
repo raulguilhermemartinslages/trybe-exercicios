097c02aefd1f1e50837ef210f5c694a571898586
Raul Guilherme Martins Lages <raul.martinslages@gmail.com>
2022-02-04 03:02:33 -0300

    inicio dos exercícios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script_teste_a_elseif.js
@@ -0,0 +1,11 @@
+var resultado = 59;
+
+if (resultado >= 80); {
+    console.log("bom");
+}
+else if (reultado < 80 && resultado >= 60); {
+    console.log("medio");
+}
+else {
+    console.log("ruim");
+}
